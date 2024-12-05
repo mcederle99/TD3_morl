c47377b324c9f9656b41010014d3f7c36966f462
Matteo Cederle <matteita99@gmail.com>
2024-12-05 20:01:37 +0100

    mini fix

--- main_mymorl.py
+++ main_mymorl.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
 	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=1e6, type=int)   # Max time steps to run environment
 	parser.add_argument("--expl_noise", default=0.1, type=float)    # Std of Gaussian exploration noise
-	parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
+	parser.add_argument("--batch_size", default=128, type=int)      # Batch size for both actor and critic
 	parser.add_argument("--discount", default=0.99, type=float)     # Discount factor
 	parser.add_argument("--tau", default=0.005, type=float)         # Target network update rate
 	parser.add_argument("--policy_noise", default=0.2)              # Noise added to target policy during critic update
